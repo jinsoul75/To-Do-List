21244c9d80b9823491022cff24be8ae5743dcc8f
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2023-01-31 13:22:34 +0000

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,9 @@
+const clock = document.querySelector(".clock");
+
+function onClock() {
+  const date = new Date();
+  clock.innerText = date.toLocaleTimeString();
+}
+
+onClock();
+setInterval(onClock, 1000);
